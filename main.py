00df44c293768bc8f04addb2afceb776280e3b34
bauti <jnemi@fie.undef.edu.ar>
2021-09-01 13:58:50 -0300

    ADD - Assignment 4c

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-def process_csv(text):
+def process_csv(input_data):
     """
 
-    :param text: string with integer, substring, and boolean separated by a comma
-    :return: list with CSV values in their correct type
+    :param input_data: string (or list of strings) with integer, substring, and boolean separated by a comma
+    :return: list (or matrix) with CSV values in their correct type
     """
-    data = text.rstrip().split(",")
-    data[0] = int(data[0])
-    data[2] = bool(data[2])
+    data = []
+    if type(input_data) == type(data):
+        for i in range(len(input_data)):
+            data.append(process_csv(input_data[i]))
+    else:
+        data = input_data.rstrip().split(",")
+        data[0] = int(data[0])
+        data[2] = bool(data[2])
     return data
 
 
-print(process_csv("14,Juana Perez,True"))
+print(process_csv(["14,Juana Perez,True", "21,Juan,True"]))
