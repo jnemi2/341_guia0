ffa64860c4f076ef50277ef2ba106acda16f29b7
bauti <jnemi@fie.undef.edu.ar>
2021-09-01 14:08:00 -0300

    ADD - Assignment 5

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import math
+
 print("Hello, world!!")
+
+
+def circle_perimeter_and_area(radius):
+    """
+
+    :param radius: real number representing the radius of the circle
+    :return: tuple with the perimeter and area of the circle
+    """
+    return math.pi * 2 * radius, math.pi * (radius ** 2)
+
+
+perimeter, area = circle_perimeter_and_area(5)
+print("Given a circle of radius {}, its perimeter is {} and its area is {}".format(5, perimeter, area))
