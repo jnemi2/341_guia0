66203e3c29b1635df74422b6057d6ea5a51afe43
bauti <jnemi@fie.undef.edu.ar>
2021-09-01 13:47:38 -0300

    ADD - Assignment 4

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello, world!!")
+def process_csv(text):
+    """
+
+    :param text: string with substrings separated by a comma
+    :return: list with substrings
+    """
+    return text.rstrip().split(",")
+
+
+print(process_csv("John,21"))
