23adcff18e55eaefe0918c684c65cc68ed917168
bauti <jnemi@fie.undef.edu.ar>
2021-09-01 10:28:19 -0300

    ADD - Assignment 1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("Hello, world!!")
+
+
+def multiple_of_7_but_not_of_5(start, end):
+    for i in range(start, end):
+        if i % 7 == 0 and i % 5 != 0:
+            print(i)
+    print()
+    for i in range(start, end):
+        if i % 7 == 0 and i % 5 != 0:
+            print(i, end=", ")
+
+
+multiple_of_7_but_not_of_5(0, 100)
