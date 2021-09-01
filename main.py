c28d31e3e014cb0c43800b3327d683cc6a6e594b
bauti <jnemi@fie.undef.edu.ar>
2021-09-01 13:51:57 -0300

    ADD - Assignment 4b

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def process_csv(text):
     """
 
-    :param text: string with substrings separated by a comma
-    :return: list with substrings
+    :param text: string with integer, substring, and boolean separated by a comma
+    :return: list with CSV values in their correct type
     """
-    return text.rstrip().split(",")
+    data = text.rstrip().split(",")
+    data[0] = int(data[0])
+    data[2] = bool(data[2])
+    return data
 
 
-print(process_csv("John,21"))
+print(process_csv("14,Juana Perez,True"))
