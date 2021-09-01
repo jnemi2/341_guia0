6a133db271b963baa87a5b3d2d2faef5f6dd88b3
bauti <jnemi@fie.undef.edu.ar>
2021-09-01 13:34:54 -0300

    ADD - Assignment 2

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello, world!!")
+def factorial(integer):
+    """
+    :type integer: int
+    """
+    result = 1
+    for i in range(1, integer+1):
+        result = i * result
+    return result
+
+
+def recursive_factorial(integer):
+    if integer <= 1:
+        return 1
+    return integer * factorial(integer - 1)
+
+
+print(factorial(7))
+print(recursive_factorial(7))
