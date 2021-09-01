688b3753da19debbfd5f61533d1eac17aff3e547
bauti <jnemi@fie.undef.edu.ar>
2021-09-01 13:40:38 -0300

    ADD - Assignment 3

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("Hello, world!!")
+
+
+def generate_keymap():
+    keymap = {}
+    for k in range(ord("a"), ord("z") + 1):
+        keymap.setdefault(chr(k), k)
+    return keymap
+
+
+print(generate_keymap())
