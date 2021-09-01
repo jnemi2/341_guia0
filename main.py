4759f3f8431f493a9054f3917dbfcab29f0acf26
bauti <jnemi@fie.undef.edu.ar>
2021-09-01 13:42:55 -0300

    ADD - Assignment 3 documentation

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ print("Hello, world!!")
 
 
 def generate_keymap():
+    """
+
+    :return: dictionary with the ASCII keymap of the alphabet
+    """
     keymap = {}
     for k in range(ord("a"), ord("z") + 1):
         keymap.setdefault(chr(k), k)
