e743fcc7856c684b180a75959bd1a9328b321d83
bauti <jnemi@fie.undef.edu.ar>
2021-09-08 09:32:42 -0300

    ADD - Assignment 6

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello, world!!")
+
+
+text = []
+print("Type 'exit' to end")
+input_text = input("Type something: ")
+while input_text.lower() != "exit":
+    text.append(input_text.capitalize())
+    input_text = input("Type something: ")
+
+for t in text:
+    print(t)
